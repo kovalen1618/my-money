363efab8eb02f3dd6cf0e8c8a0c920637978b885
kovalen1618 <kovalen1.618@gmail.com>
2023-05-07 21:51:49 -0500

    Added navbar and styles

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@
 
 import { BrowserRouter, Switch, Route } from "react-router-dom/cjs/react-router-dom";
 
+// Pages
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Signup from "./pages/signup/Signup";
+
+// Components
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Navbar />
         <Switch>
           <Route path="/">
             <Home />
