d7c7ad51625ceea79f34beb47242cf177a473428
kovalen1618 <kovalen1.618@gmail.com>
2023-05-07 21:44:00 -0500

    Added essential pages & routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-import './App.css'
+// import './App.css'
+
+import { BrowserRouter, Switch, Route } from "react-router-dom/cjs/react-router-dom";
+
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Signup from "./pages/signup/Signup";
 
 function App() {
   return (
     <div className="App">
-
+      <BrowserRouter>
+        <Switch>
+          <Route path="/">
+            <Home />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/signup">
+            <Signup/>
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
