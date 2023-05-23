335f05c9db0710bca8428190664c0eec720acaf8
kovalen1618 <kovalen1.618@gmail.com>
2023-05-23 17:02:48 -0500

    Created Login hook

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,15 +1,19 @@
 import { useState } from 'react';
+
+import { useLogin } from '../../hooks/useLogin';
+
 import styles from './Login.module.css';
 
 export default function Login() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const { login, isPending, error } = useLogin();
 
     const handleSubmit = (e) => {
         // Prevent the default behavior of the form which is to refresh the page.
         e.preventDefault();
         
-        console.log(email, password);
+        login(email, password);
     }
 
     return (
@@ -34,7 +38,9 @@ export default function Login() {
                     value={password}
                 />
             </label>
-            <button className='btn'>Login</button>
+            { !isPending && <button className='btn'>Login</button> }
+            { isPending && <button className='btn' disabled>Loading...</button> }
+            { error && <p>{ error }</p> }
         </form>
     );
 }
