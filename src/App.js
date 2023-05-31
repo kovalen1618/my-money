11e2ea1ae28f6b2a53d36a622140bc258b5aeefe
kovalen1618 <kovalen1.618@gmail.com>
2023-05-31 09:28:39 -0500

    Setup robust authentication

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@
 
 import { BrowserRouter, Switch, Route } from "react-router-dom/cjs/react-router-dom";
 
+import { useAuthContext } from "./hooks/useAuthContext";
+
 // Pages
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
@@ -11,22 +13,26 @@ import Signup from "./pages/signup/Signup";
 import Navbar from "./components/Navbar";
 
 function App() {
+  const { authIsReady } = useAuthContext();
+  
   return (
     <div className="App">
-      <BrowserRouter>
-        <Navbar />
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/login">
-            <Login />
-          </Route>
-          <Route path="/signup">
-            <Signup/>
-          </Route>
-        </Switch>
-      </BrowserRouter>
+      {authIsReady && ( // Removes flickering of screen when logging in
+        <BrowserRouter>
+          <Navbar />
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <Route path="/signup">
+              <Signup/>
+            </Route>
+          </Switch>
+        </BrowserRouter>
+      )}
     </div>
   );
 }
