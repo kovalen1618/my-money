6a5b2283fba3dada55da4f940610aac02007a877
kovalen1618 <kovalen1.618@gmail.com>
2023-06-15 10:25:01 -0500

    Setup Firebase queries and orderBy

--- src/hooks/useCollection.js
+++ src/hooks/useCollection.js
@@ -1,14 +1,28 @@
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import { projectFirestore } from "../firebase/config";
 
 
-export const useCollection = (collection) => {
+export const useCollection = (collection, _query, _orderBy) => {
     const [documents, setDocuments] = useState(null);
     const [error, setError] = useState(null);
+
+    // Placing query within a useRef stops an infinite loop from occuring due to the ref type of the query array 
+    // (its array type changes every time a new doc is added - adding new docs reevaluates everything which will loop forever with useEffect)
+    const query = useRef(_query).current;
+    const orderBy = useRef(_orderBy).current;
 
     // When collection changes, setup a new subscription to that collection
     useEffect(() => {
         let ref = projectFirestore.collection(collection)
+
+        // Query database for user-specific data
+        if (query) {
+            ref = ref.where(...query);
+        }
+
+        if (orderBy) {
+            ref = ref.orderBy(...orderBy);
+        }
 
         // Use a realtime listener with onSnaphshot
         const unsubscribe = ref.onSnapshot((snapshot) => {
@@ -30,7 +44,7 @@ export const useCollection = (collection) => {
         // Unsubscribe on unmount
         return () => unsubscribe();
 
-    }, [collection])
+    }, [collection, query, orderBy])
 
     return { documents, error }
 }
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,16 +1,26 @@
 import { useAuthContext } from '../../hooks/useAuthContext';
+import { useCollection } from '../../hooks/useCollection';
+
 import styles from './Home.module.css';
 
 // Components
 import TransactionForm from './TransactionForm';
+import TransactionList from './TransactionList';
 
 export default function Home() {
-    const { user } = useAuthContext()
+    const { user } = useAuthContext();
+    const { documents, error } = useCollection(
+        'transactions',
+        // Argument for a Firestore Database query to check if doc uid is equal to user.uid
+        ['uid', '==', user.uid],
+        ['createdAt', 'desc']
+    );
 
     return (
         <div className={styles.container}>
             <div className={styles.content}>
-                Transaction List
+                {error && <p>{error}</p>}
+                {documents && <TransactionList transactions={documents} />}
             </div>
             <div className={styles.sidebar}>
                 <TransactionForm uid={user.uid} />
